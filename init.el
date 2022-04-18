08233ffcf454be898296208d75f145a41fb59905
bello <71326435+chaiboub23@users.noreply.github.com>
2022-04-17 22:19:44 -0300

    Add files via upload

---
+++ init.el
@@ -0,0 +1,26 @@
+(add-to-list 'load-path "~/.config/emacs/ui")
+(add-to-list 'load-path "~/.config/emacs/visual")
+(add-to-list 'load-path "~/.config/emacs/packages")
+(require 'packages)
+(require 'elegance)
+(require 'sanity)
+(require 'splash)
+;; splash screen
+(show-splash)
+(setq inhibit-startup-screen t)
+;; disable tool bar, menu bar, and scroll bar
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+;; macOS command key as meta key
+(setq mac-command-modifier 'meta)
+;; make window maximized
+;; (add-hook 'window-setup-hook 'toggle-frame-maximized t)
+;; remove backup files and auto saved files
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+;; evil mode 
+(require 'evil)
+(evil-mode 1)
+
+(nano-modeline-mode)
